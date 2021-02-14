f8781bd92114a6c4a93e89beaeb3eac504e31464
Muharrem Yıldırım <muharremyildirim303@gmail.com>
2021-02-14 21:55:15 +0300

    Removed camera smooth effect on scene loaded

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -4,7 +4,7 @@ public class CameraController : MonoBehaviour
 {
 	[Header("Camera")]
 	private Rigidbody2D _localPlayerRigidbody;
-	public Vector3 CameraOffset = Vector3.zero;
+	public Vector3 CameraOffset = new Vector3(-1, 0, 0);
 
 	[SerializeField]
 	private float _followSpeed = 1f;
@@ -12,6 +12,11 @@ public class CameraController : MonoBehaviour
 	void Start()
 	{
 		_localPlayerRigidbody = GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>();
+
+		Vector3 camPos = _localPlayerRigidbody.transform.position;
+		camPos.z = transform.position.z;
+
+		transform.position = camPos + CameraOffset;
 	}
 
 	void LateUpdate()
